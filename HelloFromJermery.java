8de049c46ddfe253eee6ef2b5f1bcc22b286b90f
jalesnunes <42074619+jalesnunes@users.noreply.github.com>
2020-10-10 16:42:40 -0600

    Adding a fez lines to ask for a name and print it

--- HelloFromJermery.java
+++ HelloFromJermery.java
@@ -1,10 +1,18 @@
 package Wheeler;
 
+import java.util.Scanner;
+
 public class HelloFromJermery {
 
     public static void main(String[] args){
+        final Scanner scanner = new Scanner( System.in );
+
         System.out.println("Hello from Jermery");
         System.out.println("We'll see how this works....");
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
 
         //Here I am just practicing a simple loop in Java
 
