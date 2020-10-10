16acb96d1ec27b8501f757504b50f4b249e8059e
Jermery Wheeler <52702506+jermery614@users.noreply.github.com>
2020-10-10 18:21:57 -0400

    Add some more to this file.

--- Hello.java
+++ Hello.java
@@ -3,13 +3,17 @@ package Wheeler;
 public class Hello {
 
     public static void main(String[] args){
+        System.out.println("Hello from Jermery");
+        System.out.println("We'll see how this works....");
 
         //Here I am just practicing a simple loop in Java
 
         for (int i = 0; i < 101; i++){
             System.out.println(i);
-            // What is another way of accomplishing this in a loop?
+
 	    System.out.println("Hello from Dallen.");
+
+
         }
     }
 }
