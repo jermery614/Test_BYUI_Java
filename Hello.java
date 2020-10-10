176c9c5ec4c5bd74c89b605dba07f0f5de25d0a2
Jermery Wheeler <52702506+jermery614@users.noreply.github.com>
2020-10-09 20:51:16 -0400

    code for Jermery.

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package Wheeler;
+
+public class Hello {
+
+    public static void main(String[] args){
+
+        //Here I am just practicing a simple loop in Java
+
+        for (int i = 0; i < 101; i++){
+            System.out.println(i);
+            // What is another way of accomplishing this in a loop?
+        }
+    }
+}
