0412882cfe32500e24b1d6361a7827403884225c
Jermery Wheeler <52702506+jermery614@users.noreply.github.com>
2020-10-10 18:30:24 -0400

    Corrected the Project name.

---
+++ HelloFromJermery.java
@@ -0,0 +1,19 @@
+package Wheeler;
+
+public class HelloFromJermery {
+
+    public static void main(String[] args){
+        System.out.println("Hello from Jermery");
+        System.out.println("We'll see how this works....");
+
+        //Here I am just practicing a simple loop in Java
+
+        for (int i = 0; i < 101; i++){
+            System.out.println(i);
+
+	    System.out.println("Hello from Dallen.");
+
+
+        }
+    }
+}
